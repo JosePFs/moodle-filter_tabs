61a419c86292d50e99ee48ba76c05d9180101e9c
Alexander Bias <alexander.bias@uni-ulm.de>
2016-03-02 20:29:19 +0100

    Fix phpdoc package

--- filter.php
+++ filter.php
@@ -17,10 +17,9 @@
 /**
  * Filter "tabs"
  *
- * @package     filter
- * @subpackage  filter_tabs
- * @copyright   2013 Stefan Lehneis, University of Regensburg <stefan.lehneis@rz.uni-regensburg.de> / 2014 Alexander Bias, University of Ulm <alexander.bias@uni-ulm.de>
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    filter_tabs
+ * @copyright  2013 Stefan Lehneis, University of Regensburg <stefan.lehneis@rz.uni-regensburg.de> / 2014 Alexander Bias, University of Ulm <alexander.bias@uni-ulm.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
--- settings.php
+++ settings.php
@@ -17,10 +17,9 @@
 /**
  * Filter "tabs" - Settings
  *
- * @package     filter
- * @subpackage  filter_tabs
- * @copyright   2014 Alexander Bias, University of Ulm <alexander.bias@uni-ulm.de>
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    filter_tabs
+ * @copyright  2014 Alexander Bias, University of Ulm <alexander.bias@uni-ulm.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
